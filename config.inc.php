eacd489801f9a2f282890eef294e2f37ccf6573e
BRULE Herman <alpha_one_x86@first-world.info>
2014-01-13 23:13:25 +0100

    Minor fix

--- config.inc.php
+++ config.inc.php
@@ -7,3 +7,4 @@ $base_datapack_explorer_site_path='/official-server/datapack-explorer/';
 $base_datapack_site_path='/datapack/';
 $datapack_path='../datapack/';
 //$map_generator='/home/user/CatchChallenger/tools/build-map2pngGUI-Qt5-Debug/map2pngGUI';
+$datapack_explorer_local_path='datapack-explorer/';
--- official-server/datapack-explorer-generator.php
+++ official-server/datapack-explorer-generator.php
@@ -295,66 +295,69 @@ $industrie_meta=array();
 $industrie_link_meta=array();
 $item_produced_by=array();
 $item_consumed_by=array();
-if($handle = opendir($datapack_path.'industries/')) {
-	while(false !== ($entry = readdir($handle))) {
-	if($entry != '.' && $entry != '..') {
-			$content=file_get_contents($datapack_path.'industries/'.$entry);
-			preg_match_all('#<link[^>]+/>#isU',$content,$entry_list);
-			foreach($entry_list[0] as $entry)
-			{
-				if(!preg_match('#<link[^>]*industrialrecipe="([0-9]+)"[^>]*/>#isU',$entry))
-					continue;
-				if(!preg_match('#<link[^>]*industry="([0-9]+)"[^>]*/>#isU',$entry))
-					continue;
-				$industry_id=preg_replace('#^.*<link[^>]*industrialrecipe="([0-9]+)"[^>]*/>.*$#isU','$1',$entry);
-				$factory_id=preg_replace('#^.*<link[^>]*industry="([0-9]+)"[^>]*/>.*$#isU','$1',$entry);
-				$industrie_link_meta[$factory_id]=$industry_id;
-			}
-			preg_match_all('#<industrialrecipe[^>]+>.*</industrialrecipe>#isU',$content,$entry_list);
-			foreach($entry_list[0] as $entry)
-			{
-				if(!preg_match('#<industrialrecipe[^>]*id="([0-9]+)"#isU',$entry))
-					continue;
-				if(!preg_match('#<industrialrecipe[^>]*time="([0-9]+)"#isU',$entry))
-					continue;
-				if(!preg_match('#<industrialrecipe[^>]*cycletobefull="([0-9]+)"#isU',$entry))
-					continue;
-				$id=preg_replace('#^.*<industrialrecipe[^>]*id="([0-9]+)".*$#isU','$1',$entry);
-				$time=preg_replace('#^.*<industrialrecipe[^>]*time="([0-9]+)".*$#isU','$1',$entry);
-				$cycletobefull=preg_replace('#^.*<industrialrecipe[^>]*cycletobefull="([0-9]+)".*$#isU','$1',$entry);
-				//resource
-				$resources=array();
-				preg_match_all('#<resource[^>]+/>#isU',$entry,$temp_text_list);
-				foreach($temp_text_list[0] as $resource)
+if(is_dir($datapack_path.'industries/'))
+{
+	if($handle = opendir($datapack_path.'industries/')) {
+		while(false !== ($entry = readdir($handle))) {
+		if($entry != '.' && $entry != '..') {
+				$content=file_get_contents($datapack_path.'industries/'.$entry);
+				preg_match_all('#<link[^>]+/>#isU',$content,$entry_list);
+				foreach($entry_list[0] as $entry)
 				{
-					if(!preg_match('#<resource[^>]*id="([0-9]+)"#isU',$resource))
+					if(!preg_match('#<link[^>]*industrialrecipe="([0-9]+)"[^>]*/>#isU',$entry))
 						continue;
-					$quantity=1;
-					$item=preg_replace('#^.*<resource[^>]*id="([0-9]+)".*$#isU','$1',$resource);
-					if(!preg_match('#<resource[^>]*quantity="([0-9]+)"#isU',$resource))
-						$quantity=preg_replace('#^.*<resource[^>]*quantity="([0-9]+)".*$#isU','$1',$resource);
-					$item_consumed_by[$item][$id]=$quantity;
-					$resources[]=array('item'=>$item,'quantity'=>$quantity);
+					if(!preg_match('#<link[^>]*industry="([0-9]+)"[^>]*/>#isU',$entry))
+						continue;
+					$industry_id=preg_replace('#^.*<link[^>]*industrialrecipe="([0-9]+)"[^>]*/>.*$#isU','$1',$entry);
+					$factory_id=preg_replace('#^.*<link[^>]*industry="([0-9]+)"[^>]*/>.*$#isU','$1',$entry);
+					$industrie_link_meta[$factory_id]=$industry_id;
 				}
-				//product
-				$products=array();
-				preg_match_all('#<product[^>]+/>#isU',$entry,$temp_text_list);
-				foreach($temp_text_list[0] as $product)
+				preg_match_all('#<industrialrecipe[^>]+>.*</industrialrecipe>#isU',$content,$entry_list);
+				foreach($entry_list[0] as $entry)
 				{
-					if(!preg_match('#<product[^>]*id="([0-9]+)"#isU',$product))
+					if(!preg_match('#<industrialrecipe[^>]*id="([0-9]+)"#isU',$entry))
 						continue;
-					$quantity=1;
-					$item=preg_replace('#^.*<product[^>]*id="([0-9]+)".*$#isU','$1',$product);
-					if(!preg_match('#<product[^>]*quantity="([0-9]+)"#isU',$product))
-						$quantity=preg_replace('#^.*<product[^>]*quantity="([0-9]+)".*$#isU','$1',$product);
-					$item_produced_by[$item][$id]=$quantity;
-					$products[]=array('item'=>$item,'quantity'=>$quantity);
+					if(!preg_match('#<industrialrecipe[^>]*time="([0-9]+)"#isU',$entry))
+						continue;
+					if(!preg_match('#<industrialrecipe[^>]*cycletobefull="([0-9]+)"#isU',$entry))
+						continue;
+					$id=preg_replace('#^.*<industrialrecipe[^>]*id="([0-9]+)".*$#isU','$1',$entry);
+					$time=preg_replace('#^.*<industrialrecipe[^>]*time="([0-9]+)".*$#isU','$1',$entry);
+					$cycletobefull=preg_replace('#^.*<industrialrecipe[^>]*cycletobefull="([0-9]+)".*$#isU','$1',$entry);
+					//resource
+					$resources=array();
+					preg_match_all('#<resource[^>]+/>#isU',$entry,$temp_text_list);
+					foreach($temp_text_list[0] as $resource)
+					{
+						if(!preg_match('#<resource[^>]*id="([0-9]+)"#isU',$resource))
+							continue;
+						$quantity=1;
+						$item=preg_replace('#^.*<resource[^>]*id="([0-9]+)".*$#isU','$1',$resource);
+						if(!preg_match('#<resource[^>]*quantity="([0-9]+)"#isU',$resource))
+							$quantity=preg_replace('#^.*<resource[^>]*quantity="([0-9]+)".*$#isU','$1',$resource);
+						$item_consumed_by[$item][$id]=$quantity;
+						$resources[]=array('item'=>$item,'quantity'=>$quantity);
+					}
+					//product
+					$products=array();
+					preg_match_all('#<product[^>]+/>#isU',$entry,$temp_text_list);
+					foreach($temp_text_list[0] as $product)
+					{
+						if(!preg_match('#<product[^>]*id="([0-9]+)"#isU',$product))
+							continue;
+						$quantity=1;
+						$item=preg_replace('#^.*<product[^>]*id="([0-9]+)".*$#isU','$1',$product);
+						if(!preg_match('#<product[^>]*quantity="([0-9]+)"#isU',$product))
+							$quantity=preg_replace('#^.*<product[^>]*quantity="([0-9]+)".*$#isU','$1',$product);
+						$item_produced_by[$item][$id]=$quantity;
+						$products[]=array('item'=>$item,'quantity'=>$quantity);
+					}
+					$industrie_meta[$id]=array('time'=>$time,'cycletobefull'=>$cycletobefull,'resources'=>$resources,'products'=>$products);
 				}
-				$industrie_meta[$id]=array('time'=>$time,'cycletobefull'=>$cycletobefull,'resources'=>$resources,'products'=>$products);
 			}
 		}
+		closedir($handle);
 	}
-	closedir($handle);
 }
 
 $plant_meta=array();

4d764847b0ee2ced0a264befb4af07a9a877f145
BRULE Herman <alpha_one_x86@first-world.info>
2014-11-19 08:31:04 +0100

    Update for version .0.7.0.3

---
+++ update-version.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+if [[ `echo "$1" | sed -r "s/[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+/match/g"` == "match" ]]
+then
+	sed -i -r "s/[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+/$1/g" updater.php
+	sed -i -r "s/[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+/$1/g" download.html
+	sed -i -r "s/[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+/$1/g" official-server-org.html
+	sed -i -r "s/[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+/$1/g" shop/index.html
+	echo 'update done'
+else
+	echo "no version compatible passed: $1"
+fi

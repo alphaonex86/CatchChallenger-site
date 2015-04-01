37add40cdaa4ed90de8932878d734a6741aa9099
BRULE Herman <alpha_one_x86@first-world.info>
2015-04-01 23:18:09 +0200

    Split the datapack loading

--- datapack-list.php
+++ datapack-list.php
@@ -31,11 +31,42 @@ function listFolder($folder)
     }
     return $arr;
 }
-$folder='datapack/';
+if(!isset($_GET['main']))
+    $folder='datapack/';
+else
+{
+    if(!preg_match('#^[a-z0-9]+$#',$_GET['main']))
+        die('main with wrong char');
+    if(!isset($_GET['sub']))
+        $folder='datapack/map/main/'.$_GET['main'].'/';
+    else
+    {
+        if(!preg_match('#^[a-z0-9]+$#',$_GET['sub']))
+            die('sub with wrong char');
+        $folder='datapack/map/main/'.$_GET['main'].'/sub/'.$_GET['sub'].'/';
+    }
+}
+if(!is_dir($folder))
+    exit;
+
+/** \note substr(hash_file('sha224',$file),0,8) vs substr(hash_file('sha224',$file,true),0,4)
+Binary vs text for gzip have same size, gzip vs xz is small difference, and binary vs text file in xz the difference is minor */
+
 $arr=listFolder($folder);
 sort($arr);
-foreach($arr as $file)
-{
-    if($file!='datapack/datapack-list.txt')
-        echo str_replace($folder,'',$file).' '.substr(hash_file('sha224',$file),0,8).' '.(int)ceil(filesize($file)/1000)."\n";
-}
+if(!isset($_GET['main']))
+    foreach($arr as $file)
+    {
+        //datapack/datapack-list/base.txt
+        if(!preg_match('#^[\\/]*datapack[\\/]+datapack-list[\\/]+#',$file) && !preg_match('#^[\\/]*datapack[\\/]+map[\\/]+main[\\/]+#',$file))
+            echo str_replace($folder,'',$file).' '.substr(hash_file('sha224',$file),0,8).' '.(int)ceil(filesize($file)/1000)."\n";
+    }
+else if(!isset($_GET['sub']))
+    foreach($arr as $file)
+    {
+        if(!preg_match('#^[\\/]*datapack[\\/]+map[\\/]+main[\\/]+'.$_GET['main'].'[\\/]+sub[\\/]+#',$file))
+            echo str_replace($folder,'',$file).' '.substr(hash_file('sha224',$file),0,8).' '.(int)ceil(filesize($file)/1000)."\n";
+    }
+else
+    foreach($arr as $file)
+            echo str_replace($folder,'',$file).' '.substr(hash_file('sha224',$file),0,8).' '.(int)ceil(filesize($file)/1000)."\n";

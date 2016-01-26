21d8cf08dac60e69a5d9a26930d87f8b4da032b8
BRULE Herman <alpha_one_x86@first-world.info>
2016-01-26 01:55:00 +0100

    Add support of blacklisted password

--- register.php
+++ register.php
@@ -81,6 +81,19 @@ function send_mail($title,$text,$to,$type,$from)
 							echo '<span style="background-color:rgb(255,169,169);border:1px solid rgb(255,77,77);padding:2px;"><b>Your login can\'t be empty</b></span><br />';
 						else
                         {
+                            $handle = @fopen("blacklisted-passwords.txt", "r");
+                            $arrayofpass=array();
+                            if ($handle) {
+                                while (($buffer = fgets($handle, 4096)) !== false) {
+                                    $arrayofpass[]=$buffer;
+                                }
+                                if (!feof($handle)) {
+                                    echo "Error: unexpected fgets() fail\n";
+                                }
+                                fclose($handle);
+                            }
+                            if(in_array($_POST['password'],$arrayofpass))
+                                echo '<span style="background-color:rgb(255,169,169);border:1px solid rgb(255,77,77);padding:2px;"><b>Your password is into the most common password, it need be unique</b></span><br />';
                             if(!preg_match('#[a-z]#',$_POST['password']) || !preg_match('#[A-Z]#',$_POST['password']) || !preg_match('#[0-9]#',$_POST['password']) || strlen($_POST['password'])<6)
                                 echo '<span style="background-color:rgb(255,169,169);border:1px solid rgb(255,77,77);padding:2px;"><b>Your password need be composed of upper and lower char and number. And need be more than 6 of lenght</b></span><br />';
                             else if(!preg_match('#^[a-z0-9\.\-_]+@[a-z0-9\.\-_]+\.[a-z]{2,4}$#',$_POST['email']))

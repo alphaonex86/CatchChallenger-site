1db2d4f8f11ab2b4cc2ba2d7ece1fd6dc981edcf
BRULE Herman <alpha_one_x86@first-world.info>
2016-02-18 15:02:56 +0100

    Able to work without database

--- official-server.php
+++ official-server.php
@@ -7,6 +7,18 @@ else
     $postgres_link_site = pg_connect('dbname='.$postgres_db_site['database'].' user='.$postgres_login.' password='.$postgres_pass);
 if($postgres_link_site===FALSE)
     $is_up=false;
+
+function filewrite($file,$content)
+{
+    if($filecurs=fopen($file, 'w'))
+    {
+        if(fwrite($filecurs,$content) === false)
+            die('Unable to write the file: '.$file);
+        fclose($filecurs);
+    }
+    else
+        die('Unable to write or create the file: '.$file);
+}
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" lang="en" xml:lang="en">
@@ -65,17 +77,38 @@ if($postgres_link_site===FALSE)
                 $player_count=0;
                 $maxplayer_count=0;
                 $file='gameserver.json';
+                $previously_know_server_file='previously_know_server.json';
                 if(file_exists($file) && $filecurs=file_get_contents($file))
                 {
                     $arr=json_decode($filecurs,true);
                     if(is_array($arr))
                     {
                         ksort($arr);
+                        $previously_know_server=array();
                         if($is_up)
                         {
-                            $db_server_found=array();
                             $reply = pg_query($postgres_link_site,'SELECT uniquekey,"groupIndex",name,description FROM gameservers ORDER BY "groupIndex",uniquekey') or die(pg_last_error());
                             while($data = pg_fetch_array($reply))
+                                $previously_know_server[$data['groupIndex']][$data['uniquekey']]=$data;
+                        }
+                        else
+                        {
+                            if(file_exists($previously_know_server_file) && $filecurs=file_get_contents($previously_know_server_file))
+                            {
+                                $previously_know_server=json_decode($filecurs,true);
+                                if(!is_array($arr))
+                                    $previously_know_server=array();
+                            }
+                            else
+                                $previously_know_server=array();
+                        }
+                        ksort($previously_know_server);
+
+                        $db_server_found=array();
+                        foreach($previously_know_server as $groupIndex=>$uniqueKey_list)
+                        {
+                            ksort($uniqueKey_list);
+                            foreach($uniqueKey_list as $uniqueKey=>$data)
                             {
                                 $db_server_found[]=$data['groupIndex'].'-'.$data['uniquekey'];
                                 if(!isset($arr[$data['groupIndex']][$data['uniquekey']]))//not found
@@ -113,14 +146,25 @@ if($postgres_link_site===FALSE)
                                             echo '<li><div class="divBackground" title="'.htmlentities($description).'"><div class="labelDatapackMap"></div><strong>'.htmlentities($name).'</strong> - <span style="color:green;">online</span></div></li>';
                                         $gameserver_up++;
                                         if($data['name']!=$name || $data['description']!=$description)
-                                            pg_query($postgres_link_site,'UPDATE gameservers SET name=\''.addslashes($name).'\',description=\''.addslashes($description).'\' WHERE uniquekey='.$data['uniquekey']) or die(pg_last_error());
+                                        {
+                                            if($is_up)
+                                                pg_query($postgres_link_site,'UPDATE gameservers SET name=\''.addslashes($name).'\',description=\''.addslashes($description).'\' WHERE uniquekey='.$data['uniquekey']) or die(pg_last_error());
+                                            else
+                                            {
+                                                $previously_know_server[$groupIndex][$uniqueKey]['name']=$name;
+                                                $previously_know_server[$groupIndex][$uniqueKey]['name']=$description;
+                                            }
+                                        }
                                     }
                                     else
                                         echo '<li><div class="divBackground" title="'.htmlentities($data['description']).'"><div class="labelDatapackMap"></div><strong>'.htmlentities($data['name']).'</strong></div></li>';
                                 }
                             }
-                            foreach($arr as $groupIndex=>$server_list)
-                            foreach($server_list as $uniqueKey=>$server)
+                        }
+                        foreach($arr as $groupIndex=>$uniqueKey_list)
+                        {
+                            ksort($uniqueKey_list);
+                            foreach($uniqueKey_list as $uniqueKey=>$server)
                             {
                                 if(!in_array($groupIndex.'-'.$uniqueKey,$db_server_found))
                                 {
@@ -150,52 +194,30 @@ if($postgres_link_site===FALSE)
                                         else
                                             echo '<li><div class="divBackground" title="'.htmlentities($description).'"><div class="labelDatapackMap"></div><strong>'.htmlentities($name).'</strong> - <span style="color:green;">online</span></div></li>';
                                         $gameserver_up++;
-                                        pg_query($postgres_link_site,'INSERT INTO gameservers(uniquekey,"groupIndex",name,description) VALUES ('.addslashes($uniqueKey).','.addslashes($groupIndex).',\''.addslashes($name).'\',\''.addslashes($description).'\');') or die(pg_last_error());
+                                        if($is_up)
+                                            pg_query($postgres_link_site,'INSERT INTO gameservers(uniquekey,"groupIndex",name,description) VALUES ('.addslashes($uniqueKey).','.addslashes($groupIndex).',\''.addslashes($name).'\',\''.addslashes($description).'\');') or die(pg_last_error());
+                                        else
+                                        {
+                                            $previously_know_server[$groupIndex][$uniqueKey]=array('uniquekey'=>$uniqueKey,'groupIndex'=>$groupIndex,'name'=>$name,'description'=>$description);
+                                            ksort($previously_know_server[$groupIndex]);
+                                            ksort($previously_know_server);
+                                        }
                                     }
                                     else
                                         echo '<li><div class="divBackground"><div class="labelDatapackMap"></div><strong>Default server</strong></div></li>';
                                 }
                             }
                         }
-                        else
-                            foreach($arr as $groupIndex=>$server_list)
-                            foreach($server_list as $uniqueKey=>$server)
-                            {
-                                if(isset($server['xml']) && isset($server['connectedPlayer']) && isset($server['maxPlayer']))
-                                {
-                                    $name='';
-                                    if(preg_match('#<name( lang="en")?>.*</name>#isU',$server['xml']))
-                                    {
-                                        $name=preg_replace('#^.*<name( lang="en")?>(.*)</name>.*$#isU','$2',$server['xml']);
-                                        $name=str_replace('<![CDATA[','',str_replace(']]>','',$name));
-                                    }
-                                    if($name=='')
-                                        $name='Default server';
-                                    $description='';
-                                    if(preg_match('#<description( lang="en")?>.*</description>#isU',$server['xml']))
-                                    {
-                                        $description=preg_replace('#^.*<description( lang="en")?>(.*)</description>.*$#isU','$2',$server['xml']);
-                                        $description=str_replace('<![CDATA[','',str_replace(']]>','',$description));
-                                    }
-                                    if($server['maxPlayer']<65534 && $server['maxPlayer']>0 && $server['connectedPlayer']<=$server['maxPlayer'])
-                                    {
-                                        echo '<li><div class="divBackground" title="'.htmlentities($description).'"><div class="labelDatapackMap"></div><strong>'.htmlentities($name).'</strong> - <strong>'.$server['connectedPlayer'].'</strong>/'.$server['maxPlayer'].' players - <span style="color:green;">online</span></div></li>';
-                                        $player_count+=$server['connectedPlayer'];
-                                        $maxplayer_count+=$server['maxPlayer'];
-                                    }
-                                    else
-                                        echo '<li><div class="divBackground" title="'.htmlentities($description).'"><div class="labelDatapackMap"></div><strong>'.htmlentities($name).'</strong> - <span style="color:green;">online</span></div></li>';
-                                    $gameserver_up++;
-                                }
-                                else
-                                    echo '<li><div class="divBackground"><div class="labelDatapackMap"></div><strong>Default server</strong></div></li>';
-                            }
+                        
                         $string_array=array();
                         if($gameserver_up>0)
                             $string_array[]='<strong>'.$gameserver_up.'</strong> <span style="color:green;">online</span>';
                         if($gameserver_down>0)
                             $string_array[]='<strong>'.$gameserver_down.'</strong> <span style="color:red;">offline</span>';
                         $total_string_array[]='Game server: '.implode(', ',$string_array);
+
+                        if(!$is_up)
+                            filewrite($previously_know_server_file,json_encode($previously_know_server));
                     }
                     else
                         echo '<li><p class="text">The official server list is actually in <b>Unknown state</b>.</p></li>';

7177ad0ee74807c0e1892cfa4ab5fc007cc55762
BRULE Herman <alpha_one_x86@first-world.info>
2016-03-21 15:48:25 +0100

    Fix when empty data

--- official-server/loginserver-json-generator.php
+++ official-server/loginserver-json-generator.php
@@ -20,31 +20,49 @@ foreach($loginserverlist as $server)
     {
         $state='up';
         $contents=fread($socket,1);
-        if($contents[0x00]==0x01)
+        if(!isset($contents[0x00]))
         {
+            $state='down';
             $result[$server['host']]=array('state'=>$state,
                 'time'=>array('toconnect'=>$time,'tonegociatetheprotocol'=>$timetonegociatetheprotocol),
-            'encrypted'=>'encrypted');
+            'encrypted'=>'encrypted','error'=>'at first round no data');
         }
         else
         {
-            $tosend=hex2bin('a0019cd6498d10');
-            $time_start = microtime(true);
-            $returnsize=fwrite($socket,$tosend,2+5);
-            if($returnsize!=7)
+            if($contents[0x00]==0x01)
             {
                 $result[$server['host']]=array('state'=>$state,
-                    'time'=>array('toconnect'=>$time),
-                'encrypted'=>'clear');
+                    'time'=>array('toconnect'=>$time,'tonegociatetheprotocol'=>$timetonegociatetheprotocol),
+                'encrypted'=>'encrypted');
             }
             else
             {
-                $contents=fread($socket,1+1+4+1);
-                $time_end = microtime(true);
-                $timetonegociatetheprotocol = $time_end - $time_start;
-                $result[$server['host']]=array('state'=>$state,
-                    'time'=>array('toconnect'=>$time,'tonegociatetheprotocol'=>$timetonegociatetheprotocol),
-                'encrypted'=>'clear','returncode'=>bindec($contents[0x06]));
+                $tosend=hex2bin('a0019cd6498d10');
+                $time_start = microtime(true);
+                $returnsize=fwrite($socket,$tosend,2+5);
+                if($returnsize!=7)
+                {
+                    $result[$server['host']]=array('state'=>$state,
+                        'time'=>array('toconnect'=>$time),
+                    'encrypted'=>'clear');
+                }
+                else
+                {
+                    $contents=fread($socket,1+1+4+1);
+                    $time_end = microtime(true);
+                    $timetonegociatetheprotocol = $time_end - $time_start;
+                    if(isset($contents[0x06]))
+                        $result[$server['host']]=array('state'=>$state,
+                            'time'=>array('toconnect'=>$time,'tonegociatetheprotocol'=>$timetonegociatetheprotocol),
+                        'encrypted'=>'clear','returncode'=>bindec($contents[0x06]));
+                    else
+                    {
+                        $state='down';
+                        $result[$server['host']]=array('state'=>$state,
+                            'time'=>array('toconnect'=>$time,'tonegociatetheprotocol'=>$timetonegociatetheprotocol),
+                        'encrypted'=>'clear','error'=>'at second round no data');
+                    }
+                }
             }
         }
         fclose($socket);

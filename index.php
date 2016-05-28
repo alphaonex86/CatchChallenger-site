18864d1bcf437c45e2afaa1c6cffe39f1f46c4cd
BRULE Herman <alpha_one_x86@first-world.info>
2016-05-28 04:18:21 +0200

    Improve the logo SEO

--- index.php
+++ index.php
@@ -34,7 +34,7 @@
 				<div id="title">CatchChallenger project</div>
 				<br />
 				<br />
-				<img src="/images/pixel.png" width="96" height="96" style="float:left; margin-right:7px;" class="tiers_img" alt="" />
+				<img src="/images/catchchallenger-logo.png" width="96" height="96" style="float:left; margin-right:7px;" class="tiers_img" alt="CatchChallenger logo" title="CatchChallenger logo" />
 				<p class="text">This game is a independent <strong>MMORPG, Lan game and a single player game</strong>. You have <strong>fight, farming, explore, crafting, trading, management, competition</strong>... and you are free to play in you style.</p>
 				<p class="text">It's a pixel art and old school game. Our work is concentrated on the gameplay, creativity, performance and self hosting. The income is to paid the developing and the artwork. The game is fully <a href="https://github.com/alphaonex86/CatchChallenger">open source (<strong>GPL3</strong>)</a>.</p>
                 <p class="text">The game have no real time. That's give clear advantage on 3G/wifi connexion or into the tiers world. You can play <strong>Player vs Player, Team vs Team</strong>, your team can own city and clan hall.</p>

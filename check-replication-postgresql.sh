6139997cc76cb8c3c53564b75a638a3d5866e642
BRULE Herman <alpha_one_x86@first-world.info>
2017-02-10 14:26:02 +0100

    Add some comment

--- check-replication-postgresql.sh
+++ check-replication-postgresql.sh
@@ -10,6 +10,8 @@ fi
 
 echo Process on slave:
 CONTENT=`ssh root@catchchallenger-db-common-1-a-slave 'ps aux | grep -F "wal receiver" | grep -F -v grep | grep -F postgres | grep -F streaming'`
+# content execpted:
+# postgres 22123  0.0  0.4 154236  5032 ?        Ss   14:16   0:00 postgres: wal receiver process   streaming 0/4B00F800
 if [ "${CONTENT}" != "" ]
 then
     echo -n catchchallenger-db-common-1-a-slave:
@@ -18,6 +20,11 @@ fi
 
 echo Slave connected on master:
 CONTENT=`ssh root@catchchallenger-db-common-1-a-master 'psql -c "select * from pg_stat_replication ;" postgres' | grep -F -v backend_start | grep -v -F +-- | grep -F 172.16.0.85 | grep -F streaming`
+# raw content execpted:
+# pid | usesysid | usename | application_name | client_addr | client_hostname | client_port |         backend_start         | backend_xmin |   state   | sent_location | write_location | flush_location | replay_location | sync_priority | sync_state 
+#-----+----------+---------+------------------+-------------+-----------------+-------------+-------------------------------+--------------+-----------+---------------+----------------+----------------+-----------------+---------------+------------
+# 728 |    49152 | rep     | walreceiver      | 172.16.0.85 |                 |       38622 | 2017-02-10 14:17:33.844674+01 |              | streaming | 0/4B00F720    | 0/4B00F720     | 0/4B00F758     | 0/4B00F758      |             0 | async
+#(1 row)
 if [ "${CONTENT}" != "" ]
 then
     echo -n catchchallenger-db-common-1-a-slave:

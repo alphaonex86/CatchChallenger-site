b55651b831e511e7ed86c45e4ecd690a81c588f7
BRULE Herman <alpha_one_x86@first-world.info>
2017-06-18 21:41:34 +0200

    Improved control

--- official-server/other-json.sh
+++ official-server/other-json.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 CURRENTPATH=`pwd`
-BINPATH=/home/user/Desktop/CatchChallenger/tools/build-bot-test-connect-to-gameserver-Desktop-Debug/
+BINPATH=/home/user/Desktop/CatchChallenger/tools/build-bot-test-connect-to-gameserver-Desktop_llvm-Debug/
 cd ${BINPATH}
 
 rm -Rf ${BINPATH}/datapack/
@@ -9,31 +9,34 @@ ${BINPATH}/bot-test-connect-to-gameserver
 RETURNCODE=$?
 if [ ${RETURNCODE} -ne 0 ]
 then
+    echo "${BINPATH}/bot-test-connect-to-gameserver"
     echo '{"bot-test-connect-to-gameserver":{"state":"down","reason":"Failed at empty datapack: '${RETURNCODE}'"}}' > ${CURRENTPATH}/../other.json
     echo "Failed at empty datapack"
     exit
 fi
 
-sleep 10
+sleep 60
 
 rsync -art --delete ${BINPATH}/datapack-full/ ${BINPATH}/datapack/
 ${BINPATH}/bot-test-connect-to-gameserver
 RETURNCODE=$?
 if [ ${RETURNCODE} -ne 0 ]
 then
+    echo "${BINPATH}/bot-test-connect-to-gameserver"
     echo '{"bot-test-connect-to-gameserver":{"state":"down","reason":"Failed at full datapack: '${RETURNCODE}'"}}' > ${CURRENTPATH}/../other.json
     echo "Failed at full datapack"
     exit
 fi
 rsync -art --delete ${BINPATH}/datapack/ ${BINPATH}/datapack-full/
 
-sleep 10
+sleep 60
 
 rm ${BINPATH}/datapack/informations.xml
 ${BINPATH}/bot-test-connect-to-gameserver
 RETURNCODE=$?
 if [ ${RETURNCODE} -ne 0 ]
 then
+    echo "${BINPATH}/bot-test-connect-to-gameserver"
     echo '{"bot-test-connect-to-gameserver":{"state":"down","reason":"Failed at partial datapack: '${RETURNCODE}'"}}' > ${CURRENTPATH}/../other.json
     echo "Failed at partial datapack"
     exit

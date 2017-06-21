576947cf41d577cccdf0d96f4e015c110b722e27
BRULE Herman <alpha_one_x86@first-world.info>
2017-06-22 00:58:55 +0200

    Add more clear output include via VT100

--- official-server/other-json.sh
+++ official-server/other-json.sh
@@ -3,6 +3,10 @@ CURRENTPATH=`pwd`
 BINPATH=/home/user/Desktop/CatchChallenger/tools/build-bot-test-connect-to-gameserver-Desktop_llvm-Debug/
 cd ${BINPATH}
 
+echo '-----------------------------------------------------------'
+echo -e '-------------------\e[1mTry empty datapack\e[0m----------------------'
+echo '-----------------------------------------------------------'
+
 rm -Rf ${BINPATH}/datapack/
 mkdir ${BINPATH}/datapack/
 ${BINPATH}/bot-test-connect-to-gameserver
@@ -11,12 +15,17 @@ if [ ${RETURNCODE} -ne 0 ]
 then
     echo "${BINPATH}/bot-test-connect-to-gameserver"
     echo '{"bot-test-connect-to-gameserver":{"state":"down","reason":"Failed at empty datapack: '${RETURNCODE}'"}}' > ${CURRENTPATH}/../other.json
-    echo "Failed at empty datapack"
+    echo -e '\e[31m\e[1mFailed at empty datapack\e[39m\e[0m'
     exit
 fi
+echo -e '-------------------------Done:\e[32m\e[1mok\e[39m\e[0m---------------------------'
 
+echo -e '--------------------------\e[33m\e[1mWait\e[39m\e[0m-----------------------------'
 sleep 60
 
+echo '-----------------------------------------------------------'
+echo -e '--------------------\e[1mTry full datapack\e[0m----------------------'
+echo '-----------------------------------------------------------'
 rsync -art --delete ${BINPATH}/datapack-full/ ${BINPATH}/datapack/
 ${BINPATH}/bot-test-connect-to-gameserver
 RETURNCODE=$?
@@ -24,13 +33,18 @@ if [ ${RETURNCODE} -ne 0 ]
 then
     echo "${BINPATH}/bot-test-connect-to-gameserver"
     echo '{"bot-test-connect-to-gameserver":{"state":"down","reason":"Failed at full datapack: '${RETURNCODE}'"}}' > ${CURRENTPATH}/../other.json
-    echo "Failed at full datapack"
+    echo -e '\e[31m\e[1mFailed at full datapack\e[39m\e[0m'
     exit
 fi
-rsync -art --delete ${BINPATH}/datapack/ ${BINPATH}/datapack-full/
+echo -e '-------------------------Done:\e[32m\e[1mok\e[39m\e[0m---------------------------'
 
+echo -e '--------------------------\e[33m\e[1mWait\e[39m\e[0m-----------------------------'
 sleep 60
 
+echo '-----------------------------------------------------------'
+echo -e '--------------------\e[1mTry semi datapack\e[0m----------------------'
+echo '-----------------------------------------------------------'
+rsync -art --delete ${BINPATH}/datapack/ ${BINPATH}/datapack-full/
 rm ${BINPATH}/datapack/informations.xml
 ${BINPATH}/bot-test-connect-to-gameserver
 RETURNCODE=$?
@@ -38,9 +52,11 @@ if [ ${RETURNCODE} -ne 0 ]
 then
     echo "${BINPATH}/bot-test-connect-to-gameserver"
     echo '{"bot-test-connect-to-gameserver":{"state":"down","reason":"Failed at partial datapack: '${RETURNCODE}'"}}' > ${CURRENTPATH}/../other.json
-    echo "Failed at partial datapack"
+    echo -e '\e[31m\e[1mFailed at partial datapack\e[39m\e[0m'
     exit
 fi
+echo -e '-------------------------Done:\e[32m\e[1mok\e[39m\e[0m---------------------------'
+echo -e '\e[32m\e[1mAll is ok\e[39m\e[0m'
 
 echo '{"bot-test-connect-to-gameserver":{"state":"up"}}' > ${CURRENTPATH}/../other.json
-echo written into: ${CURRENTPATH}/../other.json
+echo -e written into: "\e[1m${CURRENTPATH}/../other.json\e[0m"

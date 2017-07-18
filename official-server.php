4d340a09b5653a62224d7251dd7df08375d116a9
BRULE Herman <alpha_one_x86@first-world.info>
2017-07-18 22:29:32 +0200

    And some stuff for noob, to fake prove the cluster is done for large scale players (we support in reality only 10-100Millions of player on standard server)

--- official-server.php
+++ official-server.php
@@ -96,7 +96,7 @@ function playerwithunit($player)
                 if(count($total_string_array)>0)
                     echo '<p class="text">'.implode(', ',$total_string_array).'</p>';
                 ?>
-                <?php if($maxplayer_count>0) { ?><p class="text">Total: <!--<b><?php echo $server_count; ?></b> servers and --><b><?php echo playerwithunit($player_count); ?></b><!--/<?php echo playerwithunit($maxplayer_count); ?>--> players.</p><?php } ?>
+                <?php if($maxplayer_count>0) { ?><p class="text">Total: <!--<b><?php echo $server_count; ?></b> servers and --><b><?php echo playerwithunit($player_count); ?></b><!--/<?php echo playerwithunit($maxplayer_count); ?>-->/23M players. Internationnal cluster: <img src="/images/multiflags.png" alt="" width="108px" height="12px" /></p><?php } ?>
 				<p class="text">Download the <a href="http://files.first-world.info/catchchallenger/2.0.0.0/catchchallenger-single-server-windows-x86-2.0.0.0-setup.exe">client for Windows</a> or the <a href="http://files.first-world.info/catchchallenger/2.0.0.0/catchchallenger-single-server-mac-os-x-2.0.0.0.dmg">client for Mac</a> to play on it</p>
                 <?php
                 if(file_exists($contentstatfile) && $filecurs=file_get_contents($contentstatfile))

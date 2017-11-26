236ba3d508f8dea8d843f76b4f303e0241132519
BRULE Herman <alpha_one_x86@first-world.info>
2017-11-27 00:02:54 +0100

    Fix some bug

--- backup-parse.php
+++ backup-parse.php
@@ -177,10 +177,14 @@ if(file_exists($logfile))
 {
     if($filecurs=file_get_contents($logfile))
     {
-        if(strpos($filecurs, 'already running')===NULL && strpos($filecurs, 'no vm found')===NULL && strpos($filecurs, 'internal error')===NULL)
-            $returnVar[$backup]=array('state'=>'up');
-        else
+        if(strpos($filecurs, 'already running')!==FALSE)
             $returnVar[$backup]=array('state'=>'down','reason'=>'backup already running');
+        else if(strpos($filecurs, 'no vm found')!==FALSE)
+            $returnVar[$backup]=array('state'=>'down','reason'=>'no vm found');
+        else if(strpos($filecurs, 'internal error')!==FALSE)
+            $returnVar[$backup]=array('state'=>'down','reason'=>'internal error');
+        else
+            $returnVar[$backup]=array('state'=>'up');
     }
     else
         $returnVar[$backup]=array('state'=>'down','reason'=>($logfile.' not readable or empty'));

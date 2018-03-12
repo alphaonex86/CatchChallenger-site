adc485b0be60c1ae326a8d70719ec1965c3c32a9
BRULE Herman <alpha_one_x86@first-world.info>
2018-03-12 04:14:23 +0100

    Fixe versions

--- update-version.sh
+++ update-version.sh
@@ -5,6 +5,7 @@ then
 	sed -i -r "s/[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+/$1/g" download.html
 	sed -i -r "s/[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+/$1/g" official-server-org.html
 	sed -i -r "s/[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+/$1/g" shop/index.html
+	sed -i -r "s/[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+/$1/g" official-server.php
 	echo 'update done'
 else
 	echo "no version compatible passed: $1"

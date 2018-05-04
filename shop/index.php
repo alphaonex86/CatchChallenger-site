54b6de821fb321fd2ca913530a956f3b979b9ab5
BRULE Herman <alpha_one_x86@first-world.info>
2018-05-04 17:04:28 +0200

    Improve the shop

--- shop/index.php
+++ shop/index.php
@@ -1,7 +1,7 @@
 <?php
 $title='CatchChallenger shop - Old school Opensource MMORPG/Single player with multiple gameplay';
-$description='CatchChallenger shop, Independent Old school Opensource MMORPG/Single player game';
-$keywords='catchchallenger,catch challenger,catch challenger,shop';
+$description='CatchChallenger shop to buy CatchChallenger Ultimate, Independent Old school Opensource MMORPG/Single player game';
+$keywords='catchchallenger,catch challenger,catch challenger,shop, CatchChallenger Ultimate, pokemon';
 include '../template/top.php';
 include '../template/top2.php';
 ?>
@@ -11,12 +11,23 @@ include '../template/top2.php';
         With the <i>Ultimate version</i> you have:
         <ul>
             <li><b>Better interface</b>, nicer with more informations</li>
-            <li>You help the software to keep it as open source software</li>
             <li><b>Priority support</b>, features requests, bug fix</li>
-            <li><b>Access to all server</b>, not only the official server, you can connect to your own server: Simple server for: <a href="http://files.first-world.info/catchchallenger/2.0.4.3/catchchallenger-server-windows-x86-2.0.4.3.7z">Windows</a>, <a href="http://files.first-world.info/catchchallenger/2.0.4.3/catchchallenger-server-windows-x86-2.0.4.3.zip">Mac</a><br /></li>
+            <li><b>Access to all server</b>, not only the official server, <b>you can connect to your own server</b></li>
         </ul>
 		<img src="/images/bitcoin.png" alt="" style="float:left"  width="16px" height="16px" /><span style="font-size:150%;font-weight: bold;"><a href="http://shop.first-world.info/">Buy your key on the shop</a></span> (paypal and bitcoin), and download:<br />
-        Download link for: <a href="http://files.first-world.info/catchchallenger/2.0.4.3/catchchallenger-ultimate-windows-x86-2.0.4.3-setup.exe">Windows</a>, <a href="http://files.first-world.info/catchchallenger/2.0.4.3/catchchallenger-ultimate-mac-os-x-2.0.4.3.zip">Mac</a><br />
+        
+   				<table>
+                    <tr>
+                        <td class="tiers_img bigbutton" style="width:162px"><center><img src="/images/ultimate.png" width="96" height="96" alt="" />Ultimate</center></td>
+                        <td class="tiers_img bigbutton"><a href="http://files.first-world.info/catchchallenger/2.0.4.3/catchchallenger-ultimate-windows-x86-2.0.4.3-setup.exe"><center><img src="/images/windows.png" width="96" height="96" alt="" />Windows</center></a></td>
+                        <td class="tiers_img bigbutton"><a href="http://files.first-world.info/catchchallenger/2.0.4.3/catchchallenger-ultimate-mac-os-x-2.0.4.3.zip"><center><img src="/images/mac.png" width="96" height="96" alt="" />Mac</center></a></td>
+                    </tr>
+                    <tr>
+                        <td class="tiers_img bigbutton" style="width:162px"><center><img src="/images/server.png" width="96" height="96" alt="" />Your server</center></td>
+                        <td class="tiers_img bigbutton"><a href="http://files.first-world.info/catchchallenger/2.0.4.3/catchchallenger-server-windows-x86-2.0.4.3.7z"><center><img src="/images/windows.png" width="96" height="96" alt="" />Windows</center></a></td>
+                        <td class="tiers_img bigbutton"><a href="http://files.first-world.info/catchchallenger/2.0.4.3/catchchallenger-server-windows-x86-2.0.4.3.zip"><center><img src="/images/mac.png" width="96" height="96" alt="" />Mac</center></a></td>
+                    </tr>
+                </table>
 
 <?php
 include '../template/bottom2.php';

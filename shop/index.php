4f908c655842d4240edff599794bca8fa3ddc251
BRULE Herman <alpha_one_x86@first-world.info>
2018-05-04 17:19:06 +0200

    Better shop images

--- shop/index.php
+++ shop/index.php
@@ -1,7 +1,7 @@
 <?php
 $title='CatchChallenger shop - Old school Opensource MMORPG/Single player with multiple gameplay';
 $description='CatchChallenger shop to buy CatchChallenger Ultimate, Independent Old school Opensource MMORPG/Single player game';
-$keywords='catchchallenger,catch challenger,catch challenger,shop, CatchChallenger Ultimate, pokemon';
+$keywords='catchchallenger,catch challenger,catch challenger,shop, CatchChallenger Ultimate, pokemon, private server, server, MMORPG';
 include '../template/top.php';
 include '../template/top2.php';
 ?>
@@ -18,12 +18,12 @@ include '../template/top2.php';
         
    				<table>
                     <tr>
-                        <td class="tiers_img bigbutton" style="width:162px"><center><img src="/images/ultimate.png" width="96" height="96" alt="" />Ultimate</center></td>
+                        <td class="tiers_img bigbutton" style="width:162px"><center><img src="/images/ultimate.png" width="96" height="96" alt="CatchChallenger Ultimate" title="CatchChallenger Ultimate" />Ultimate</center></td>
                         <td class="tiers_img bigbutton"><a href="http://files.first-world.info/catchchallenger/2.0.4.3/catchchallenger-ultimate-windows-x86-2.0.4.3-setup.exe"><center><img src="/images/windows.png" width="96" height="96" alt="" />Windows</center></a></td>
                         <td class="tiers_img bigbutton"><a href="http://files.first-world.info/catchchallenger/2.0.4.3/catchchallenger-ultimate-mac-os-x-2.0.4.3.zip"><center><img src="/images/mac.png" width="96" height="96" alt="" />Mac</center></a></td>
                     </tr>
                     <tr>
-                        <td class="tiers_img bigbutton" style="width:162px"><center><img src="/images/server.png" width="96" height="96" alt="" />Your server</center></td>
+                        <td class="tiers_img bigbutton" style="width:162px"><center><img src="/images/server.png" width="96" height="96" alt="CatchChallenger private server" title="Your personal CatchChallenger private server" />Your server</center></td>
                         <td class="tiers_img bigbutton"><a href="http://files.first-world.info/catchchallenger/2.0.4.3/catchchallenger-server-windows-x86-2.0.4.3.7z"><center><img src="/images/windows.png" width="96" height="96" alt="" />Windows</center></a></td>
                         <td class="tiers_img bigbutton"><a href="http://files.first-world.info/catchchallenger/2.0.4.3/catchchallenger-server-windows-x86-2.0.4.3.zip"><center><img src="/images/mac.png" width="96" height="96" alt="" />Mac</center></a></td>
                     </tr>

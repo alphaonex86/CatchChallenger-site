15c3852be1f9f17bdc6a21d29069359f97313d04
BRULE Herman <alpha_one_x86@first-world.info>
2018-05-04 04:05:00 +0200

    Minor improvements and animation on home page

--- index.php
+++ index.php
@@ -17,30 +17,28 @@ This game is a independent MMORPG or a single player game. You can fight, farmin
     <tr>
         <td><img src="/images/free-to-play.png" width="267" height="104" alt="Free to play open source" title="Free to play open source" /></td>
         <td><a href="/rules.html"><img src="/images/rules.png" width="267" height="104" alt="Rules of Catch challenger" title="Rules of Catch challenger" /></a></td>
-        <td id="download"><a href="/download.html"><img src="/images/download.png" width="266" height="116" alt="Download this open source MMORPG" title="Download this open source MMORPG" /></a></td>
+        <td id="download"><a href="/download.html"><img src="/images/d.png" width="266" height="116" alt="Download this open source MMORPG" title="Download this open source MMORPG" /><img src="/images/a.png" width="59" height="43" alt="" class="bounce" /></a></td>
     </tr>
 </table>
 </section>
 
 <section class="subsec">
-    <div class="header sh"><a href="/screenshot.html"><span class="fh">Screen</span>shot</a></div>
-    <div class="mscreenl"><a href="/screenshot/catchchallenger-ingame.png"><img src="/screenshot/catchchallenger-ingame-mini.jpg" class="screenshot" alt="ingame into the mmorpg client" title="ingame into the mmorpg client" width="200" height="150" /></a>
-
+    <div class="header"><a href="/screenshot.html"><span class="fh">Screen</span>shot</a></div>
+    <div class="mscreenl">
+    <a href="/screenshot/catchchallenger-ingame.png"><img src="/screenshot/catchchallenger-ingame-mini.jpg" class="screenshot" alt="ingame into the mmorpg client" title="ingame into the mmorpg client" width="200" height="150" /></a>
     <a href="/screenshot/catchchallenger-battle.png"><img src="/screenshot/catchchallenger-battle-mini.png" class="screenshot" alt="battle" title="battle" width="200" height="150" /></a>
-
     <a href="/screenshot/catchchallenger-crafting.png"><img src="/screenshot/catchchallenger-crafting-mini.jpg" class="screenshot" alt="catchchallenger crafting" title="catchchallenger crafting" width="200" height="150" /></a>
-
     <br style="clear:both;" />
     </div>
 </section>
 
 <?php
-date_default_timezone_set('Europe/Paris');
+/*date_default_timezone_set('Europe/Paris');
 $filecurs=file_get_contents('http://catchchallenger.first-world.info/forum/feed.php?f=7');
 if(preg_match('#^.*<content[^>]*>(.*)</content>.*$#isU',$filecurs))
 {
     echo '    <section class="subsec">
-    <div class="header sh"><a href="/forum/viewforum.php?f=7"><span class="fh">Ne</span>ws</a></div>
+    <div class="header"><a href="/forum/viewforum.php?f=7"><span class="fh">Ne</span>ws</a></div>
     <div class="bodynews"><p class="text">';
     $filecurs=preg_replace('#^.*<content[^>]*>(.*)</content>.*$#isU','$1',$filecurs);
     $filecurs=preg_replace('#<p>Statistics:.*$#isU','',$filecurs);
@@ -52,7 +50,7 @@ if(preg_match('#^.*<content[^>]*>(.*)</content>.*$#isU',$filecurs))
     echo htmlspecialchars_decode($filecurs);
     echo '</p>    </div>
 </section>';
-}
+}*/
 ?>
 
 <?php

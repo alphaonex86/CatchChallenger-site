8846d6155e9259571ff472f51081448bd0f72340
BRULE Herman <alpha_one_x86@first-world.info>
2019-01-02 20:43:20 -0400

    [Improvement] Merge binary and shop

--- download.php
+++ download.php
@@ -6,26 +6,18 @@ include 'template/top.php';
 include 'template/top2.php';
 ?>
 
-				<div id="title">Download the version: 2.0.4.3</div>
+				<div id="title">Download <b>CatchChallenger</b></div>
 				<br />
 				<br />
 				<table>
                     <tr>
-                        <td class="tiers_img bigbutton" style="width:162px"><center><img src="/images/pixel.png" width="96" height="96" alt="" />Single player</center></td>
-                        <td class="tiers_img bigbutton"><a href="http://files.first-world.info/catchchallenger/2.0.4.3/catchchallenger-single-player-windows-x86-2.0.4.3-setup.exe"><center><img src="/images/windows.png" width="96" height="96" alt="" />Windows</center></a></td>
-                        <td class="tiers_img bigbutton"><a href="http://files.first-world.info/catchchallenger/2.0.4.3/catchchallenger-single-player-mac-os-x-2.0.4.3.zip"><center><img src="/images/mac.png" width="96" height="96" alt="" />Mac</center></a></td>
-                    </tr>
-                    <tr>
-                        <td class="tiers_img bigbutton" style="width:162px"><center><img src="/images/pixel2.png" width="96" height="96" alt="" />MMORPG</center></td>
-                        <td class="tiers_img bigbutton"><a href="http://files.first-world.info/catchchallenger/2.0.4.3/catchchallenger-single-server-windows-x86-2.0.4.3-setup.exe"><center><img src="/images/windows.png" width="96" height="96" alt="" />Windows</center></a></td>
-                        <td class="tiers_img bigbutton"><a href="http://files.first-world.info/catchchallenger/2.0.4.3/catchchallenger-single-server-mac-os-x-2.0.4.3.zip"><center><img src="/images/mac.png" width="96" height="96" alt="" />Mac</center></a></td>
+                        <td class="tiers_img bigbutton"><a href="http://files.first-world.info/catchchallenger/2.2.0.0/catchchallenger-windows-x86-2.2.0.0-setup.exe"><center><img src="/images/windows.png" width="96" height="96" alt="" />Windows</center></a></td>
+                        <td class="tiers_img bigbutton"><a href="http://files.first-world.info/catchchallenger/2.2.0.0/catchchallenger-mac-os-x-2.2.0.0.zip"><center><img src="/images/mac.png" width="96" height="96" alt="" />Mac</center></a></td>
                     </tr>
                 </table>
 				<br />
 				<br />
-				<a href="/shop/"><b>Ultracopier Ultimate</b> (MMORPG personal server + Extra) on <b>the shop</b></a><br />
-				The sources of the client/server: <a href="https://github.com/alphaonex86/CatchChallenger">https://github.com/alphaonex86/CatchChallenger</a> and 
-				<a href="http://files.first-world.info/catchchallenger/2.0.4.3/catchchallenger-src-2.0.4.3.tar.xz">catchchallenger-src-2.0.4.3.tar.xz</a><br />
+				The sources of the client/server: <a href="https://github.com/alphaonex86/CatchChallenger">https://github.com/alphaonex86/CatchChallenger</a><br />
 
 <?php
 include 'template/bottom2.php';
